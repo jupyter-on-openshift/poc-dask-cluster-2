14945773c36713d84b8a9122eb9c4f06388003b2
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-05-27 07:36:49 +1000

    Add Dask monitor as JupyterHub service.

--- jupyterhub/.jupyter/jupyterhub_config.py
+++ jupyterhub/.jupyter/jupyterhub_config.py
@@ -3,3 +3,10 @@ import os
 c.KubeSpawner.environment = dict(
     DASK_SCHEDULER_ADDRESS=os.environ['DASK_SCHEDULER_ADDRESS']
 )
+
+c.JupyterHub.services = [
+    {
+	'name': 'dask-monitor',
+	'url': 'http://%s' % os.environ['DASK_MONITOR_ADDRESS']
+    }
+]
