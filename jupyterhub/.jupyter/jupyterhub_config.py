b5f94d7c6093541247b2b910732d6d86d00ee98b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-05-28 14:45:02 +1000

    Add support for OAuth with persistent volumes.

--- jupyterhub/.jupyter/jupyterhub_config.py
+++ jupyterhub/.jupyter/jupyterhub_config.py
@@ -10,3 +10,62 @@ c.JupyterHub.services = [
 	'url': 'http://%s' % os.environ['DASK_MONITOR_ADDRESS']
     }
 ]
+
+enable_persistent_volumes = False
+
+if os.environ.get('OAUTH_SERVICE_TYPE') == 'GitHub':
+    from oauthenticator.github import GitHubOAuthenticator
+    c.JupyterHub.authenticator_class = GitHubOAuthenticator
+    enable_persistent_volumes = True
+
+c.MyOAuthenticator.oauth_callback_url = os.environ.get('OAUTH_CALLBACK_URL' )
+c.MyOAuthenticator.client_id = os.environ.get('OAUTH_CLIENT_ID')
+c.MyOAuthenticator.client_secret = os.environ.get('OAUTH_CLIENT_SECRET')
+
+if enable_persistent_volumes:
+    c.KubeSpawner.user_storage_pvc_ensure = True
+
+    c.KubeSpawner.pvc_name_template = '%s-nb-{username}' % \
+             c.KubeSpawner.hub_connect_ip
+
+    c.KubeSpawner.user_storage_capacity = '1Gi'
+
+    c.KubeSpawner.volumes = [
+	{
+	    'name': 'data',
+	    'persistentVolumeClaim': {
+		'claimName': c.KubeSpawner.pvc_name_template
+	    }
+	}
+    ]
+
+    c.KubeSpawner.volume_mounts = [
+	{
+	    'name': 'data',
+	    'mountPath': '/opt/app-root',
+	    'subPath': 'app-root'
+	}
+    ]
+
+    c.KubeSpawner.singleuser_init_containers = [
+	{
+	    'name': 'setup-volume',
+	    'image': os.environ['JUPYTERHUB_NOTEBOOK_IMAGE'],
+	    'command': [
+		'setup-volume.sh',
+		'/opt/app-root',
+		'/mnt/app-root'
+	    ],
+	    'resources': {
+		'limits': {
+		    'memory': '256Mi'
+		}
+	    },
+	    'volumeMounts': [
+		{
+		    'name': 'data',
+		    'mountPath': '/mnt'
+		}
+	    ]
+	}
+    ]
